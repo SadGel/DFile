527f9cc5bc1c70a34286277349bf3c5d242e3a0d
SadGel <gel78@mail.ru>
2017-07-27 15:22:20 +0400

    v1

--- src/Main_Run.java
+++ src/Main_Run.java
@@ -14,7 +14,7 @@ public class Main_Run {
      * Created by User15 on 27.07.2017.
      */
 
-        public static void saveUrl(final String filename, final String urlString)
+        public static void saveUrl(final String filename, final String urlString) //123
                 throws MalformedURLException, IOException {
             BufferedInputStream in = null;
             FileOutputStream fout = null;
